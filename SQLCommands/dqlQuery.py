ade2c66a77f2bff0fd5613e922e9fb59ac42e89c
marianavns <mariana.vns@gmail.com>
2021-09-14 08:32:56 -0300

    (refactor): change select keys

--- SQLCommands/dqlQuery.py
+++ SQLCommands/dqlQuery.py
@@ -1,7 +1,9 @@
 def returnQueryLinguagens ():
-    queryLinguagens = "SELECT * FROM aplicativoajudae.linguagem"
+    queryLinguagens = "SELECT pk_Nome FROM aplicativoajudae.linguagem"
     return queryLinguagens
 
 def returnQueryLinguagem ():
-    queryLinguagem = "SELECT idDesafio, Linguagem_Do_Desafio, Descricao, Criado_Por FROM aplicativoajudae.desafio WHERE Linguagem_Do_Desafio LIKE '%{}%' AND Solucionado = 0"
+    queryLinguagem = "SELECT idDesafio, Linguagem_Do_Desafio, Titulo, Criado_Por FROM aplicativoajudae.desafio WHERE Linguagem_Do_Desafio LIKE '%{}%' AND Solucionado = 0"
     return queryLinguagem
+
+
